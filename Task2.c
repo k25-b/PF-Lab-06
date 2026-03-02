cd075c5c6841cf1348f6d4b5e5e17077bd27a0b8
k25-b <k256572@nu.edu.pk>
2026-03-02 11:14:32 +0500

    Create Task2.c

---
+++ Task2.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int total_data = 1000, data_usage = 0;
+int main() {
+    while (total_data > 0) {
+        printf("Enter data usage: ");
+        scanf("%d", &data_usage);
+        if (data_usage > total_data) {
+            printf("You cannot use more than total data.\n");
+            data_usage = 0;
+            printf("Enter again: ");
+            scanf("%d", &data_usage);
+        }
+        else if (total_data < 0) {
+            printf("Data plan exceeded!\n");
+            data_usage = 0;
+            printf("Enter data in your plan: \n");
+            scanf("%d", &data_usage);
+        }
+        total_data = total_data - data_usage;
+        printf("Data left: %d \n", total_data);
+    }}
