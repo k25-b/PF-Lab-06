51caa074a51f8cd4df509f4fc10485f2a30bdd72
k25-b <k256572@nu.edu.pk>
2026-03-02 11:42:11 +0500

    Update Task5.c
    While loop does not need a condition in this case, as I have already used an explicit break. ---> Fixed 
    
    Sentinel Value Check First ----> Fixed

--- Task5.c
+++ Task5.c
@@ -1,17 +1,21 @@
 #include<stdio.h>
 int total_units, daily_units;
 int main() {
-    while (daily_units != -999) {
+    while (1) {
         printf("Enter consumed units: \n");
         scanf("%d", &daily_units);
-        if (daily_units < 0 && daily_units != -999) {
-            printf("Daily consumed units must be non-negative.\n");
-            daily_units = 0;
-        }
         if (daily_units == -999){
             break;
         }
+        if (daily_units < 0) {
+            printf("Daily consumed units must be non-negative.\n");
+            daily_units = 0;
+        }
         total_units += daily_units;
+
     }
     printf("Total units consumed: %d", total_units);
+
+
+
 }
